652637f849b54569423b21a05a734f13bc37806b
isayaeli <isayaelib@gmail.com>
2022-04-27 18:25:18 +0300

    backend updates

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     name =  models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)
     message = models.TextField()
 
 
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+import imp
+from django.shortcuts import redirect, render
+from .models import Project
+from django.contrib import messages
 # Create your views here.
 def index(request):
+    project = Project()
     if request.method == 'POST':
-        request.POST['name']
-        request.POST['email']
-        request.POST['title']
-        request.POST['message']
+        project.name = request.POST['name']
+        project.phone = request.POST['phone']
+        project.email = request.POST['email']
+        project.title = request.POST['title']
+        project.message = request.POST['message']
+        project.save()
+        messages.success(request, "Your Information has been successful submited")
+        return redirect('index')
+        
     return render(request, 'index.html')
