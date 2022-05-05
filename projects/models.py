11c13b0c1ce67436fc9eb5709ad0b528f86c3c7d
isayaeli <isayaelib@gmail.com>
2022-05-05 15:26:33 +0300

    track time

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,9 @@
 import email
 from email import message
 from operator import mod
+from pyexpat import model
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Project(models.Model):
     name =  models.CharField(max_length=100)
@@ -10,6 +11,7 @@ class Project(models.Model):
     phone = models.CharField(max_length=100)
     title = models.CharField(max_length=100, null=True, blank=True)
     message = models.TextField()
+    submited_on = models.DateTimeField(default=datetime.now)
 
 
     def __str__(self):
